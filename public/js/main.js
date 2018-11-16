6e5abda3048086a943b6aa260b4cca3c259dcefb
tuannx21 <xuan.tuan@1pac.vn>
2018-11-16 16:41:44 +0700

    update section service

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,4 @@
-(function () {
+(function() {
   'use strict';
   console.log('hello');
 })();
-
-const hover = () => {
-  classRed = querySelectorAll('.klassbac')
-  classRed.forEach((ele)=>{
-    ele.re
-  })
-}
